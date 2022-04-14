2ad64b2f02cfd3e908f31c96ad386d2d234876c2
Stefano Rondelli <rstefanofil@hotmail.com>
2022-04-14 20:01:16 +0200

    Add wincons

--- lib/connect_four.rb
+++ lib/connect_four.rb
@@ -1,31 +1,11 @@
-class Grid
-    attr_accessor :grid
+module Grid_Navigation
 
-    def initialize
-        @grid = make_grid
-    end
-
-    def make_grid
-        Array.new(42, " ")
-    end
-
-    def display_grid
-        i = 0
-        b = 0
-        until i == 6 && b == 42
-            print "| " + @grid.slice(b, 7).join(" | ").to_s + " |\n"
-            # print " | " + @grid.slice(b, 7).map { | element | element.class.ancestors.include?(Token) ? element.symbol : element }.join(" | ").to_s + " |\n"
-            print " ---+---+---+---+---+---+---\n"
-            i += 1
-            b += 7
-        end
-        print "  1   2   3   4   5   6   7\n"
-    end
+    LEFT_BORDER = [0, 7, 14, 21, 28, 35]
+    RIGHT_BORDER = [6, 13, 20, 27, 34, 41]
 
     def define_columns
         columns = Array.new
         k = 0
-        i = 0
         until k == 7 do
             columns << define_single_column(k)
             k += 1
@@ -39,14 +19,122 @@ class Grid
         single_column
     end
 
-    def place_token(column)
+    def define_win_conditions_full
+        win_conditions = Array.new
+        win_conditions.concat(define_win_conditions_vertical).concat(define_win_conditions_horizontal).concat(define_win_conditions_diagonal_left).concat(define_win_conditions_diagonal_right)
+    end
 
+    def define_win_conditions_vertical
+        win_conditions_vertical = Array.new
+        k = 0
+        until k > 20 do
+            win_con = [k]
+            3.times { win_con << win_con.last + 7}
+            win_conditions_vertical << win_con
+            k += 1
+        end
+        win_conditions_vertical
+    end
+
+    def define_win_conditions_horizontal
+        win_conditions_horizontal = Array.new
+        k = 0
+        until k > 41 do
+            win_con = [k]
+            3.times { win_con << win_con.last + 1}
+            win_conditions_horizontal << win_con
+            k += 1
+        end
+        win_conditions_horizontal = prune_border_crossing_horizontal(win_conditions_horizontal)
+    end
+
+    def prune_border_crossing_horizontal(array)
+        array.map! do | element |
+            if RIGHT_BORDER.include?(element[0]) || RIGHT_BORDER.include?(element[1]) || RIGHT_BORDER.include?(element[2])
+                element = nil
+            else
+                element
+            end
+        end
+        array.compact!
+    end
+
+    def define_win_conditions_diagonal_right
+        win_conditions_diagonal_right = Array.new
+        k = 0
+        until k > 17 do
+            win_con = [k]
+            3.times { win_con << win_con.last + 8}
+            win_conditions_diagonal_right << win_con
+            k += 1
+        end
+        win_conditions_diagonal_right = prune_border_crossing_diagonal(win_conditions_diagonal_right)
+    end
+
+    def define_win_conditions_diagonal_left
+        win_conditions_diagonal_left = Array.new
+        k = 0
+        until k > 20 do
+            win_con = [k]
+            3.times { win_con << win_con.last + 6}
+            win_conditions_diagonal_left << win_con
+            k += 1
+        end
+        win_conditions_diagonal_left = prune_border_crossing_diagonal(win_conditions_diagonal_left)
+    end
+
+    def prune_border_crossing_diagonal(array)
+        array.map! do | element |
+            if RIGHT_BORDER.include?(element[1]) || RIGHT_BORDER.include?(element[2]) || LEFT_BORDER.include?(element[1]) || LEFT_BORDER.include?(element[2])
+                element = nil
+            else
+                element
+            end
+        end
+        array.compact!
+    end
+
+end
+
+class Grid
+    attr_accessor :grid
+    include Grid_Navigation
+
+    def initialize
+        @grid = make_grid
+    end
+
+    def make_grid
+        Array.new(42, " ")
+    end
+
+    def display_grid
+        i = 0
+        b = 0
+        until i == 6 && b == 42
+            print "| " + @grid.slice(b, 7).map { | element | element.class == Token ? element.symbol : element }.join(" | ").to_s + " |\n"
+            print " ---+---+---+---+---+---+---\n"
+            i += 1
+            b += 7
+        end
+        print "  1   2   3   4   5   6   7\n"
     end
 
 end
 
 class Game
 
+    def start_game
+        grid = Grid.new
+        player_one, player_two = make_players
+        play_round(grid, player_one, player_two)
+    end
+
+    def play_round(grid, player_one, player_two)
+        player_one.place_token(grid, choose_placement) if turn.odd?
+        player_two.place_token(grid, choose_placement) if !turn.odd?
+        grid.display_grid
+    end
 
     def make_players_dummy
         player_one = Player.new("Fran", "F")
@@ -58,6 +146,7 @@ class Game
         symbol_one, symbol_two = symbols_input(name_one, name_two)
         player_one = Player.new(name_one, symbol_one)
         player_two = Player.new(name_two, symbol_two)
+        return player_one, player_two
     end
 
     def names_input
@@ -92,30 +181,62 @@ class Game
         return symbol_one, symbol_two
     end
 
+    def choose_placement
+        puts "Pick the column in which you want to place your token."
+        col = gets.chomp
+        until col.length == 1 && col >= 1 && col <= 7 && check_placement_validity(col)
+            puts "Invalid column."
+            col = gets.chomp
+        end
+    end
+
+    def check_placement_validity(col)
+
+    end
+
 end
 
 class Player
-    @@id = 1
+    attr_accessor :name, :symbol
+
+    include Grid_Navigation
 
     def initialize(name = nil, symbol = nil)
         @name = name
         @symbol = symbol
-        @number = @@id
-        @@id += 1
+    end
+
+    def place_token(grid, column)
+        columns = define_columns
+        target = columns[column - 1]
+        target.reverse.each do | element |
+            if grid.grid[element] == " "
+                grid.grid[element] = Token.new(self.symbol)
+                return
+            end
+        end
     end
 
 end
 
 class Token
+    attr_reader :symbol, :total
 
-    def initialize(aymbol = nil)
+    @@total = 0
+
+    def initialize(symbol = nil)
         @symbol = symbol
+        @@total += 1
     end
 
 end
 
 
-# board = Grid.new
+board = Grid.new
+board.define_win_conditions_full
+# player_one = Player.new("Fran", "F")
+# player_two = Player.new("Kolb", "K")
 # board.display_grid
-# board.define_columns
-# puts "STOP"
+# player_one.place_token(board, 2)
+# board.display_grid
+puts "STOP"
--- spec/connect_four_spec.rb
+++ spec/connect_four_spec.rb
@@ -1,21 +1,52 @@
 require "./lib/connect_four.rb"
 
 describe Grid do
-    subject(:grid) { described_class.new }
+    subject(:board) { described_class.new }
 
     describe "#define_columns" do
         context "creates array for columns" do
-            it "return column 2" do
-                array = grid.define_columns
+            it "returns column 2" do
+                array = board.define_columns
                 expect(array[1]).to eq([1, 8, 15, 22, 29, 36])
             end
 
-            it "return column 7" do
-                array = grid.define_columns
+            it "returns column 7" do
+                array = board.define_columns
                 expect(array[6]).to eq([6, 13, 20, 27, 34, 41])
             end
         end
     end
 
+end
+
+describe Player do
+    subject(:player) { described_class.new("Fran", "F") }
+    
+
+
+    describe "#place_token" do
+        context "inserts token when a column is picked from an empty board" do
+            before do
+                board = class_double("Grid").as_stubbed_const
+            end
+
+            it "returns a token at the bottom of column 2" do
+                player.place_token(board, 2)
+                expect(board.grid[36]).to eq("F")
+            end
+        end
+
+        context "inserts token on top of another" do
+            before do
+                board = class_double("Grid").as_stubbed_const
+            end
+
+            it "returns a token on top of another in column 5" do
+                board.grid[39] = "F"
+                player.place_token(board, 5)
+                expect(board.grid[32]).to eq("F")
+            end
+        end
+    end
 
 end
